0da4b8d9641489f1ba10050a39dbcc93009d8b74
Alper Reha Yazgan <alperreha@gmail.com>
2023-12-18 10:04:12 +0300

    devcontainer updated due to build err

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 go mod download
+
+# install golang task runner
+# install golang task, mage and gopls
+# go get -u -v github.com/go-task/task/cmd/task 2>&1
+# go get -u -v github.com/magefile/mage 2>&1
+# go get -u -v golang.org/x/tools/cmd/gopls 2>&1
+
