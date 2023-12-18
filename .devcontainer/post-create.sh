8ffa0b9ac743efa6b0513ecbdac89fe90804eab3
Alper Reha Yazgan <alperreha@gmail.com>
2023-12-18 13:48:37 +0300

    dev container compose network err fix, psotcreate updated

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,8 +3,8 @@
 go mod download
 
 # install golang task runner
+go install  github.com/go-task/task/cmd/task 2>&1
 # install golang task, mage and gopls
-# go get -u -v github.com/go-task/task/cmd/task 2>&1
-# go get -u -v github.com/magefile/mage 2>&1
+go install github.com/magefile/mage 2>&1
 # go get -u -v golang.org/x/tools/cmd/gopls 2>&1
 
